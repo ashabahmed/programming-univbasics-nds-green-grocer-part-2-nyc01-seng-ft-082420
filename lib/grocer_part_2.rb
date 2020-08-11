6d799a1e1dc8c2f104e22522f77234cf7d1e68fc
Ashab Ahmed <ashabahmed7@gmail.com>
2020-08-11 02:10:36 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -38,6 +38,7 @@ def checkout(cart, coupons)
   
   consolidate_cart(cart)
   apply_coupons(cart, coupons)
+  apply_clearance(cart)
   # Consult README for inputs and outputs
   #
   # This method should call
