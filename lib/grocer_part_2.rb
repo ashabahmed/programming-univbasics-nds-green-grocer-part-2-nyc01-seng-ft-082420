d9e793bf236c0c77aecad03632db87069bf8b975
Ashab Ahmed <ashabahmed7@gmail.com>
2020-08-11 02:17:31 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -47,6 +47,7 @@ def checkout(cart, coupons)
   #
   # BEFORE it begins the work of calculating the total (or else you might have
   # some irritated customers
+  {:item=>"BEETS", :price=>2.5, :clearance=>false, :count=>1}
   binding.pry
   total = 
 end
