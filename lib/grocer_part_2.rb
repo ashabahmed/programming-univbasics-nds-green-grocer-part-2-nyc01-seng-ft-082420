9b1042ddd88654602d8d48ac9b927bfb071e5ba4
Ashab Ahmed <ashabahmed7@gmail.com>
2020-08-11 02:49:16 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -38,7 +38,7 @@ def checkout(cart, coupons)
   
   consolidate_cart(cart)
   apply_coupons(cart, coupons)
-  apply_clearance(cart)
+  
 
  total = 0 
   cart.each do |item| total += item[:price]*item[:count]
