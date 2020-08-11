d103d3facd718b0ddf6da59f14477845c8b57645
Ashab Ahmed <ashabahmed7@gmail.com>
2020-08-11 02:19:42 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -50,7 +50,7 @@ def checkout(cart, coupons)
   cart = [{:item=>"BEETS", :price=>2.5, :clearance=>false, :count=>6},
   {:item=>"DICK", :price=>4.5, :clearance=>false, :count=>3}
   ]
-  cart.each {}
+  cart.each {|item|}
   binding.pry
   total = 
 end
