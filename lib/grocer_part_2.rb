fc7161a1cf21e59a87aeff3b6de0272a976e5fe7
Ashab Ahmed <ashabahmed7@gmail.com>
2020-08-11 01:12:49 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -8,7 +8,8 @@ def apply_coupons(cart, coupons)
     if discount_item
       if discount_item[:count] >= coupon[:num]
         
-        
+        {:item => "AVOCADO", :num => 2, :cost => 5.00}
+        {:item => "Burgers", :num => 3, :cost => 15.00}
         
         
         new_hash = {:item => "#{coupon[:item]} W/COUPON",
