3054a388565304ff3fe90ae2cc46f3262843edf1
Ashab Ahmed <ashabahmed7@gmail.com>
2020-08-11 01:18:09 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -7,15 +7,13 @@ def apply_coupons(cart, coupons)
     
     if discount_item
       if discount_item[:count] >= coupon[:num]
-        
-        
+ 
         cart << {:item => "#{coupon[:item]} W/COUPON",
           :price => coupon[:cost]/coupon[:num],
           :clearance => true,
           :count => coupon[:num]
         }
 
-        
       end
     end
     
