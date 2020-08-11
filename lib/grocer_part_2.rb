88a196afc2049c4e1cb3a6071a2b712ec52dbfe9
Ashab Ahmed <ashabahmed7@gmail.com>
2020-08-11 02:16:35 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -47,6 +47,6 @@ def checkout(cart, coupons)
   #
   # BEFORE it begins the work of calculating the total (or else you might have
   # some irritated customers
-  
+  binding.pry
   total = nil
 end
